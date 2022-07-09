94125ee938601f8e7ef381b3b6f1c3732a2419f0
NyiNyiZin3 <nyinyizin213@gmail.com>
2022-07-09 14:41:28 +0630

    update

--- app/Http/Requests/StorecategoryRequest.php
+++ app/Http/Requests/StorecategoryRequest.php
@@ -4,7 +4,7 @@ namespace App\Http\Requests;
 
 use Illuminate\Foundation\Http\FormRequest;
 
-class StorecatagoryRequest extends FormRequest
+class StorecategoryRequest extends FormRequest
 {
     /**
      * Determine if the user is authorized to make this request.
@@ -24,7 +24,7 @@ class StorecatagoryRequest extends FormRequest
     public function rules()
     {
         return [
-            return ['name' => 'required|unique:categories'];
+            'name' => 'required|unique:categories'
         ];
     }
 }
