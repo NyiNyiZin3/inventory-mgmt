d0a46c1a3ed6b028224ad4db654f9d95f96fee15
NyiNyiZin3 <nyinyizin213@gmail.com>
2022-07-09 14:28:55 +0630

    store category req

--- app/Http/Requests/StorecategoryRequest.php
+++ app/Http/Requests/StorecategoryRequest.php
@@ -13,7 +13,7 @@ class StorecatagoryRequest extends FormRequest
      */
     public function authorize()
     {
-        return false;
+        return true;
     }
 
     /**
@@ -24,7 +24,7 @@ class StorecatagoryRequest extends FormRequest
     public function rules()
     {
         return [
-            //
+            return ['name' => 'required|unique:categories'];
         ];
     }
 }
