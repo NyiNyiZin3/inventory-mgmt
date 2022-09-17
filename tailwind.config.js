e9e7c393e715cb460b49c159b2e56b31f0d83059
NyiNyiZin3 <nyinyizin213@gmail.com>
2022-09-17 14:07:53 +0630

    tailwind

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,5 +1,6 @@
 /** @type {import('tailwindcss').Config} */
 const defaultTheme = require('tailwindcss/defaultTheme');
+const colors = require('tailwindcss/colors');
 
 module.exports = {
     content: [
@@ -12,6 +13,34 @@ module.exports = {
         extend: {
             fontFamily: {
                 sans: ['Nunito', ...defaultTheme.fontFamily.sans],
+            },
+            colors: {
+                transparent: 'transparent',
+                current: 'currentColor',
+                amber: colors.amber,
+                black: colors.black,
+                blue: colors.blue,
+                cyan: colors.cyan,
+                emerald: colors.emerald,
+                fuchsia: colors.fuchsia,
+                gray: colors.trueGray,
+                blueGray: colors.blueGray,
+                coolGray: colors.coolGray,
+                trueGray: colors.trueGray,
+                warmGray: colors.warmGray,
+                green: colors.green,
+                indigo: colors.indigo,
+                lime: colors.lime,
+                orange: colors.orange,
+                pink: colors.pink,
+                purple: colors.purple,
+                red: colors.red,
+                rose: colors.rose,
+                sky: colors.sky,
+                teal: colors.teal,
+                violet: colors.violet,
+                yellow: colors.amber,
+                white: colors.white,
             },
         },
     },
